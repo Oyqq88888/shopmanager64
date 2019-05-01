e707d6f3ec7bbe88f94f1d2c67eabaad7682324f
Oyqq88888 <Oyqq88888@163.com>
2019-05-02 00:27:58 +0800

    处理了登陆请求的响应

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="login-wrap">
-    <el-form label-position="top" label-width="80px" :model="formData">
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
       <h2>用户登陆</h2>
       <el-form-item label="用户">
         <el-input v-model="formData.username"></el-input>
@@ -8,7 +8,9 @@
       <el-form-item label="密码">
         <el-input v-model="formData.password"></el-input>
       </el-form-item>
-      <el-button type="success">登陆</el-button>
+      <el-button
+      @click.prevent="handleLogin()"
+      class="login-btn" type="success">登陆</el-button>
 
     </el-form>
   </div>
@@ -24,9 +26,42 @@ export default {
       }
 
     }
+  },
+  methods: {
+    handleLogin () {
+      this.$http.post(`login`, this.formData)
+      .then((res) => {
+        console.log(res)
+        const {data: {data,meta: {msg, status}}} = res
+        if (status === 200) {
+          this.$message({
+          message: '登陆成功',
+          type: 'success'
+        })
+        } else {
+          this.$message.error('msg')
+        }
+      })
+    }
   }
 }
 </script>
 
 <style>
+.login-wrap {
+  height: 100%;
+  background-color: #324152;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-form {
+  background-color: #ffffff;
+  border-radius: 5px;
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
+}
 </style>
