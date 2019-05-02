91d0ef2cb9b5a6595e45363079719452fbdaf99e
Oyqq88888 <Oyqq88888@163.com>
2019-05-02 15:59:55 +0800

    头部调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,18 @@
 <template>
   <el-container class="container">
-    <el-header>Header</el-header>
+    <el-header>
+      <el-row>
+        <el-col :span="4">
+          <img src="../assets/logo.png" alt="">
+        </el-col>
+        <el-col :span="19" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="logout">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
     <el-container>
       <el-aside width="200px" class="aside">Aside</el-aside>
       <el-main class="main">Main</el-main>
@@ -16,12 +28,21 @@ export default {};
 <style>
 .container {
   height: 100%;
-  background-color: red;
+  background-color: #B3C0D1;
 }
 .aside {
   background-color: pink;
 }
 .main {
   background-color: blue;
+}
+.middle {
+  line-height: 60px;
+  text-align: center;
+  color: #ffffff;
+}
+.logout {
+  line-height: 60px;
+  text-decoration: none;
 }
 </style>
